5fc5e5c32846d490ba79aeeb0c5965c5a6defcb4
Anand-S23 <anandsroopsingh23@gmail.com>
2023-03-16 20:44:10 -0400

    Moving axum into server directory

---
+++ server/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{routing::get, Router, response::Json};
+use std::net::SocketAddr;
+use serde_json::{Value, json};
+
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(json));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
